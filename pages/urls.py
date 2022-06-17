70216c9430ef235b641275ba2b23dfd7b3541a3d
Kiran <kgawas9@gmail.com>
2022-06-17 20:14:41 +0530

    set up template and configured navigation links

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='homePage'),
+    path('about', views.about, name='about'),
+    path('services', views.services, name='services'),
+    path('contact-us', views.contact, name='contact-us')
 ]
