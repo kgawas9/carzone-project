5686ca74a09bfdbf73b36188ffea726f909a2582
Kiran <kgawas9@gmail.com>
2022-06-19 23:25:40 +0530

    modification in car details and pagination for cars page

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
+from .models import Car
 # Create your views here.
 
 def cars(request):
-    return render(request, 'cars/cars.html')
+    cars_queryset = Car.objects.order_by('-created_at')
+    paginator = Paginator(cars_queryset, 4)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
+
+    context = {
+        'cars': paged_cars,
+    }
+    return render(request, 'cars/cars.html', context=context)
+
+
+def car_details(request, id):
+    car = get_object_or_404(Car, pk=id)
+
+    context = {
+        'car_details': car,
+    }
+
+    return render(request, 'cars/car_details.html', context=context)
